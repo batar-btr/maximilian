4d52a9e4728e66ae09f94f8fbba71169637383fc
unknown <btrn86@gmail.com>
2021-09-30 15:15:58 +0300

    add clock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Clock from './components/clock/clock'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Clock/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/clock/clock.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+const Clock = props => {
+    let [date, setDate] = useState(new Date());
+
+    function tick() {
+        setDate(new Date());
+    }
+
+    useEffect(() => {
+        let timerID = setInterval(() => tick(), 1000);
+
+        return () => clearInterval(timerID)
+    },[]);
+
+    return (
+        <div>
+            <p>{date.toLocaleTimeString()}</p>
+        </div>
+    );
+}
+
+export default Clock;
